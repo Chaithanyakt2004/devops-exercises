f0a7ec6aec4b2813c1b63a1f5089477df8a0c9f9
Chaithanyakt2004 <155985246+Chaithanyakt2004@users.noreply.github.com>
2024-07-03 07:40:41 +0530

    Fibonnaci.py

--- coding/python/binary_search.py
+++ coding/python/binary_search.py
@@ -1,28 +1,9 @@
-#!/usr/bin/env python
-
-import random
-from typing import List
-
-
-def binary_search(arr: List[int], lb: int, ub: int, target: int) -> int:
-    """
-    A Binary Search Example which has O(log n) time complexity.
-    """
-    if lb <= ub:
-        mid: int = lb + (ub - lb) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            return binary_search(arr, mid + 1, ub, target)
-        else:
-            return binary_search(arr, lb, mid - 1, target)
+def fib(num):
+    if num<=0:
+        return 0
+    elif num==1:
+        return 1
     else:
-        return -1
-
-
-if __name__ == '__main__':
-    rand_num_li: List[int] = sorted([random.randint(1, 50) for _ in range(10)])
-    target: int = random.randint(1, 50)
-    print("List: {}\nTarget: {}\nIndex: {}".format(
-        rand_num_li, target,
-        binary_search(rand_num_li, 0, len(rand_num_li) - 1, target)))
+        return fib(num-1)+fib(num-2)
+num=int(input("Enter a Number:"))
+print("Fibonacci of given number",num,"is:",fib(num))
